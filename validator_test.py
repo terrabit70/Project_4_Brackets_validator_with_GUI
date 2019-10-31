cebddddfde5b6ead862babfa05d7fd8c9a10f113
Terrabit70 <ivgen_hunt@mail.ru>
2019-10-31 17:33:39 +0700

    Added unittest for validator

---
+++ validator_test.py
@@ -0,0 +1,30 @@
+import unittest
+from validator import Validator
+
+class ValidatorTests(unittest.TestCase):
+    def test_is_valid_single_pair_brackets(self):
+        validator = Validator('()')
+        self.assertTrue(validator._is_valid('My test text ( with brackets ) '))
+        self.assertFalse(validator._is_valid('()sldmfsl;j('))
+
+        validator = Validator('[]')
+        self.assertTrue(validator._is_valid('My test text [ with brackets ] '))
+        self.assertFalse(validator._is_valid('[]]cxcvsd$[]'))
+
+        validator = Validator('{}')
+        self.assertTrue(validator._is_valid('My test text { with brackets } '))
+        self.assertFalse(validator._is_valid('{}cjlfsdla9*#}'))
+
+        validator = Validator('<>')
+        self.assertTrue(validator._is_valid('My test text < with brackets > '))
+        self.assertFalse(validator._is_valid('>><klfshkjho**&<'))
+
+    def test_is_valid_many_pairs_brackets(self):
+        validator = Validator('()[]{}<>')
+        self.assertTrue(validator._is_valid('My test text [{<( with brackets )>}] '))
+        self.assertFalse(validator._is_valid('()}{[]>>)'))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
